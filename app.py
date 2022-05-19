24c793be508974882a0d035f55cba6e73e2c4667
Lais Stefani <39139861+LaisStefani@users.noreply.github.com>
2022-05-19 15:30:05 -0300

    Bug Add

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-    
+    return "Laboratório Pipeline DevOps - Lais"
+
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
     try:                                                                                                                                          
